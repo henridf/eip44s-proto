eda5b275fb6599fc0f6d056df209a584e671dcfd
Henri DF <henridf@gmail.com>
2022-04-16 11:16:37 +0200

    Bump size again

--- spec/spec.go
+++ spec/spec.go
@@ -2,9 +2,9 @@ package spec
 
 // go run sszgen/*.go --path ../../work/eip4444/
 
-const MaxBlocks = 1000000
+const MaxBlocks = 2000000
 const MaxBlockSize = 268435456
 
 type Blocks struct {
-	RlpPayload [][]byte `ssz-max:"1000000,268435456"`
+	RlpPayload [][]byte `ssz-max:"2000000,268435456"`
 }
